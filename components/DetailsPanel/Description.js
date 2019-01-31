af2baf95aec358d79017fd80f938194115e1c156
joakimia <joakimamundsen91@gmail.com>
2019-01-31 08:45:52 +0000

    5c57b0c remove description max length check in DetailsPanel as this potentially removes relevant information

--- components/DetailsPanel/Description.js
+++ components/DetailsPanel/Description.js
@@ -5,28 +5,15 @@ import { Parser as RichTextParser } from '@dhis2/d2-ui-rich-text';
 
 export var Description = function Description(_ref) {
     var description = _ref.description;
-
-    var descriptionMaxLength = 250;
-
-    if (!description) {
-        return React.createElement(
-            'i',
-            null,
-            i18n.t('No description')
-        );
-    } else if (description.length < descriptionMaxLength) {
-        return React.createElement(
-            RichTextParser,
-            null,
-            description
-        );
-    } else {
-        return React.createElement(
-            RichTextParser,
-            null,
-            description.substring(0, descriptionMaxLength) + ' ...'
-        );
-    }
+    return description ? React.createElement(
+        RichTextParser,
+        null,
+        description
+    ) : React.createElement(
+        'i',
+        null,
+        i18n.t('No description')
+    );
 };
 
 export default Description;
