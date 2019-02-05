ca21329d287dce385aa42aca53b14dbeb683fe6c
Edoardo Sabadelli <edoardo@dhis2.org>
2019-02-05 09:51:37 +0000

    d20d87d DHIS2-5906: fix handling of user select for mentions

---
+++ components/interpretations/InterpretationDialog.js
@@ -0,0 +1,168 @@
+import _extends from 'babel-runtime/helpers/extends';
+import _Object$getPrototypeOf from 'babel-runtime/core-js/object/get-prototype-of';
+import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
+import _createClass from 'babel-runtime/helpers/createClass';
+import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructorReturn';
+import _inherits from 'babel-runtime/helpers/inherits';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import i18n from '@dhis2/d2-i18n';
+import SharingDialog from '@dhis2/d2-ui-sharing-dialog';
+import MentionsWrapper from '@dhis2/d2-ui-mentions-wrapper';
+
+var styles = {
+    dialog: {
+        width: 550
+    }
+};
+
+var InterpretationDialog = function (_Component) {
+    _inherits(InterpretationDialog, _Component);
+
+    function InterpretationDialog() {
+        var _ref;
+
+        var _temp, _this, _ret;
+
+        _classCallCheck(this, InterpretationDialog);
+
+        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+            args[_key] = arguments[_key];
+        }
+
+        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = InterpretationDialog.__proto__ || _Object$getPrototypeOf(InterpretationDialog)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
+            value: _this.props.interpretation.text,
+            sharingDialogIsOpen: false,
+            savedInterpretation: null
+        }, _this.cancel = function () {
+            _this.props.onClose();
+        }, _this.onUserSelect = function (newValue) {
+            _this.setState({ value: newValue });
+        }, _this.onChange = function (event) {
+            _this.setState({ value: event.target.value });
+        }, _this.save = function () {
+            return _this._saveInterpretation().then(function (savedInterpretation) {
+                _this.props.onSave(savedInterpretation);
+                _this.props.onClose();
+            });
+        }, _this.saveAndShare = function () {
+            return _this._saveInterpretation().then(function (savedInterpretation) {
+                _this.props.onSave(savedInterpretation);
+                _this.setState({ savedInterpretation: savedInterpretation, sharingDialogIsOpen: true });
+            });
+        }, _temp), _possibleConstructorReturn(_this, _ret);
+    }
+
+    _createClass(InterpretationDialog, [{
+        key: '_saveInterpretation',
+        value: function _saveInterpretation() {
+            var interpretation = this.props.interpretation;
+            var value = this.state.value;
+
+            interpretation.text = value;
+            return interpretation.save();
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var d2 = this.context.d2;
+            var _props = this.props,
+                interpretation = _props.interpretation,
+                classes = _props.classes;
+            var _state = this.state,
+                value = _state.value,
+                sharingDialogIsOpen = _state.sharingDialogIsOpen,
+                savedInterpretation = _state.savedInterpretation;
+
+            var isActionEdit = !!interpretation.id;
+            var title = isActionEdit ? i18n.t('Edit interpretation') : i18n.t('Create interpretation');
+            var buttonProps = { color: 'primary', disabled: !value };
+
+            if (sharingDialogIsOpen) {
+                return React.createElement(SharingDialog, {
+                    open: true,
+                    onRequestClose: this.cancel,
+                    d2: d2,
+                    id: savedInterpretation.id,
+                    type: 'interpretation'
+                });
+            } else {
+                return React.createElement(
+                    Dialog,
+                    {
+                        open: true,
+                        onClose: this.cancel,
+                        maxWidth: 'md',
+                        classes: { paper: classes.dialog }
+                    },
+                    React.createElement(
+                        DialogTitle,
+                        null,
+                        title
+                    ),
+                    React.createElement(
+                        DialogContent,
+                        null,
+                        React.createElement(
+                            MentionsWrapper,
+                            { d2: d2, onUserSelect: this.onUserSelect },
+                            React.createElement(
+                                FormControl,
+                                { fullWidth: true },
+                                React.createElement(TextField, {
+                                    name: 'interpretation',
+                                    value: value,
+                                    margin: 'normal',
+                                    multiline: true,
+                                    rowsMax: 4,
+                                    onChange: this.onChange
+                                })
+                            )
+                        )
+                    ),
+                    React.createElement(
+                        DialogActions,
+                        null,
+                        React.createElement(
+                            Button,
+                            { color: 'primary', onClick: this.cancel },
+                            i18n.t('Cancel')
+                        ),
+                        !isActionEdit && React.createElement(
+                            Button,
+                            _extends({}, buttonProps, { onClick: this.saveAndShare }),
+                            i18n.t('Save & share')
+                        ),
+                        React.createElement(
+                            Button,
+                            _extends({}, buttonProps, { onClick: this.save }),
+                            i18n.t('Save')
+                        )
+                    )
+                );
+            }
+        }
+    }]);
+
+    return InterpretationDialog;
+}(Component);
+
+InterpretationDialog.propTypes = {
+    interpretation: PropTypes.object.isRequired,
+    onSave: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired
+};
+
+InterpretationDialog.contextTypes = {
+    d2: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(InterpretationDialog);
---
+++ components/interpretations/InterpretationsCard.js
@@ -0,0 +1,253 @@
+import _Object$getPrototypeOf from "babel-runtime/core-js/object/get-prototype-of";
+import _classCallCheck from "babel-runtime/helpers/classCallCheck";
+import _createClass from "babel-runtime/helpers/createClass";
+import _possibleConstructorReturn from "babel-runtime/helpers/possibleConstructorReturn";
+import _inherits from "babel-runtime/helpers/inherits";
+import _extends from "babel-runtime/helpers/extends";
+import React from "react";
+import PropTypes from "prop-types";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import AddIcon from "@material-ui/icons/Add";
+import i18n from "@dhis2/d2-i18n";
+import orderBy from "lodash/fp/orderBy";
+
+import CollapsibleCard from "../CollapsibleCard";
+import InterpretationDialog from "./InterpretationDialog";
+import Interpretation from "./Interpretation";
+import InterpretationModel from "../../models/interpretation";
+import styles from "./InterpretationsStyles.js";
+
+var getInterpretationsList = function getInterpretationsList(props) {
+    var d2 = props.d2,
+        model = props.model,
+        interpretations = props.interpretations,
+        setCurrentInterpretation = props.setCurrentInterpretation,
+        onChange = props.onChange;
+
+
+    return interpretations.length === 0 ? React.createElement(
+        "div",
+        { style: { fontStyle: "italic" } },
+        React.createElement(
+            "span",
+            null,
+            i18n.t("No interpretations")
+        )
+    ) : interpretations.map(function (interpretation, i) {
+        return React.createElement(
+            "div",
+            {
+                key: interpretation.id,
+                style: i === interpretations.length - 1 ? _extends({}, styles.interpretation, {
+                    paddingBottom: 0
+                }) : styles.interpretation,
+                className: "interpretation-box",
+                onClick: function onClick() {
+                    return setCurrentInterpretation(interpretation.id);
+                }
+            },
+            React.createElement(Interpretation, {
+                d2: d2,
+                model: model,
+                interpretation: interpretation,
+                onChange: onChange,
+                extended: false,
+                onSelect: setCurrentInterpretation
+            })
+        );
+    });
+};
+
+var getInterpretationDetails = function getInterpretationDetails(props) {
+    var d2 = props.d2,
+        model = props.model,
+        setCurrentInterpretation = props.setCurrentInterpretation,
+        interpretation = props.interpretation,
+        onChange = props.onChange;
+
+
+    return React.createElement(Interpretation, {
+        d2: d2,
+        model: model,
+        interpretation: interpretation,
+        onChange: onChange,
+        extended: true,
+        onSelect: setCurrentInterpretation
+    });
+};
+
+var getInterpretationButtons = function getInterpretationButtons(props) {
+    var currentInterpretation = props.currentInterpretation,
+        setCurrentInterpretation = props.setCurrentInterpretation,
+        openNewInterpretationDialog = props.openNewInterpretationDialog;
+
+
+    return currentInterpretation ? React.createElement(
+        IconButton,
+        {
+            onClick: function onClick() {
+                return setCurrentInterpretation(null);
+            },
+            title: i18n.t("Clear interpretation")
+        },
+        React.createElement(ChevronLeftIcon, null)
+    ) : React.createElement(
+        IconButton,
+        {
+            onClick: openNewInterpretationDialog,
+            title: i18n.t("Write new interpretation")
+        },
+        React.createElement(AddIcon, null)
+    );
+};
+
+var InterpretationsCard = function (_React$Component) {
+    _inherits(InterpretationsCard, _React$Component);
+
+    function InterpretationsCard(props) {
+        _classCallCheck(this, InterpretationsCard);
+
+        var _this = _possibleConstructorReturn(this, (InterpretationsCard.__proto__ || _Object$getPrototypeOf(InterpretationsCard)).call(this, props));
+
+        _this.state = {
+            interpretationToEdit: null,
+            currentInterpretationId: props.currentInterpretationId
+        };
+
+        _this.notifyChange = _this.notifyChange.bind(_this);
+        _this.openNewInterpretationDialog = _this.openNewInterpretationDialog.bind(_this);
+        _this.closeInterpretationDialog = _this.closeInterpretationDialog.bind(_this);
+        _this.setCurrentInterpretation = _this.setCurrentInterpretation.bind(_this);
+        _this.isControlledComponent = !!props.onCurrentInterpretationChange;
+        return _this;
+    }
+
+    _createClass(InterpretationsCard, [{
+        key: "componentWillReceiveProps",
+        value: function componentWillReceiveProps(nextProps) {
+            if (this.isControlledComponent) {
+                this.setState({
+                    currentInterpretationId: nextProps.currentInterpretationId
+                });
+            }
+        }
+    }, {
+        key: "componentDidMount",
+        value: function componentDidMount() {
+            var currentInterpretation = this.getCurrentInterpretation();
+            if (currentInterpretation && this.props.onCurrentInterpretationChange) {
+                this.props.onCurrentInterpretationChange(currentInterpretation);
+            }
+            if (this.props.currentInterpretationId == "new") {
+                this.openNewInterpretationDialog();
+            }
+        }
+    }, {
+        key: "notifyChange",
+        value: function notifyChange() {
+            this.props.onChange();
+        }
+    }, {
+        key: "openNewInterpretationDialog",
+        value: function openNewInterpretationDialog() {
+            var newInterpretation = new InterpretationModel(this.props.model, {});
+            this.setState({ interpretationToEdit: newInterpretation });
+        }
+    }, {
+        key: "closeInterpretationDialog",
+        value: function closeInterpretationDialog() {
+            this.setState({ interpretationToEdit: null });
+        }
+    }, {
+        key: "setCurrentInterpretation",
+        value: function setCurrentInterpretation(interpretationId) {
+            var _props = this.props,
+                model = _props.model,
+                onCurrentInterpretationChange = _props.onCurrentInterpretationChange;
+
+
+            if (this.isControlledComponent) {
+                var currentInterpretation = interpretationId ? model.interpretations.find(function (interpretation) {
+                    return interpretation.id === interpretationId;
+                }) : null;
+                onCurrentInterpretationChange(currentInterpretation);
+            } else {
+                this.setState({ currentInterpretationId: interpretationId });
+            }
+        }
+    }, {
+        key: "getCurrentInterpretation",
+        value: function getCurrentInterpretation() {
+            var model = this.props.model;
+            var currentInterpretationId = this.state.currentInterpretationId;
+
+            return model && model.interpretations && currentInterpretationId ? model.interpretations.find(function (interpretation) {
+                return interpretation.id === currentInterpretationId;
+            }) : null;
+        }
+    }, {
+        key: "render",
+        value: function render() {
+            var model = this.props.model;
+            var interpretationToEdit = this.state.interpretationToEdit;
+            var d2 = this.context.d2;
+
+            var sortedInterpretations = orderBy(["created"], ["desc"], model.interpretations);
+            var currentInterpretation = this.getCurrentInterpretation();
+            var actions = getInterpretationButtons({
+                d2: d2,
+                model: model,
+                currentInterpretation: currentInterpretation,
+                setCurrentInterpretation: this.setCurrentInterpretation,
+                openNewInterpretationDialog: this.openNewInterpretationDialog
+            });
+
+            return React.createElement(
+                CollapsibleCard,
+                {
+                    title: i18n.t("Interpretations"),
+                    actions: actions
+                },
+                interpretationToEdit && React.createElement(InterpretationDialog, {
+                    model: model,
+                    interpretation: interpretationToEdit,
+                    onSave: this.notifyChange,
+                    onClose: this.closeInterpretationDialog
+                }),
+                React.createElement(
+                    "div",
+                    { style: { margin: 12 } },
+                    currentInterpretation ? getInterpretationDetails({
+                        d2: d2,
+                        model: model,
+                        interpretation: currentInterpretation,
+                        setCurrentInterpretation: this.setCurrentInterpretation,
+                        onChange: this.notifyChange
+                    }) : getInterpretationsList({
+                        d2: d2,
+                        model: model,
+                        interpretations: sortedInterpretations,
+                        setCurrentInterpretation: this.setCurrentInterpretation,
+                        onChange: this.notifyChange
+                    })
+                )
+            );
+        }
+    }]);
+
+    return InterpretationsCard;
+}(React.Component);
+
+InterpretationsCard.propTypes = {
+    model: PropTypes.object.isRequired,
+    currentInterpretationId: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    onCurrentInterpretationChange: PropTypes.func
+};
+
+InterpretationsCard.contextTypes = {
+    d2: PropTypes.object.isRequired
+};
+
+export default InterpretationsCard;
