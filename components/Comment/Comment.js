2f05a7363ed342018d57a97385ff3be3cb364563
joakimia <joakimamundsen91@gmail.com>
2019-02-27 08:33:39 +0000

    7be2f04 pass onUserSelect instead of onInputchange to mentionswrapper

---
+++ components/Comment/Comment.js
@@ -0,0 +1,85 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import i18n from '@dhis2/d2-i18n';
+import ActionButton from '../Buttons/ActionButton';
+import WithAvatar from '../Avatar/WithAvatar';
+import CardHeader from '../Cards/CardHeader';
+import CardText from '../Cards/CardText';
+import CardInfo from '../Cards/CardInfo';
+import DeleteDialog from '../DeleteDialog/DeleteDialog';
+import { formatDate } from '../../dateformats/dateformatter';
+import styles from './styles/Comment.style';
+
+export var Comment = function Comment(_ref) {
+    var classes = _ref.classes,
+        comment = _ref.comment,
+        isOwner = _ref.isOwner,
+        canReply = _ref.canReply,
+        locale = _ref.locale,
+        onEdit = _ref.onEdit,
+        onReply = _ref.onReply,
+        onDelete = _ref.onDelete,
+        dialogIsOpen = _ref.dialogIsOpen,
+        onDeleteConfirm = _ref.onDeleteConfirm,
+        onDeleteCancel = _ref.onDeleteCancel;
+    return React.createElement(
+        Fragment,
+        null,
+        React.createElement(
+            WithAvatar,
+            { className: classes.comment, key: comment.id, firstName: comment.user.firstName, surname: comment.user.surname },
+            React.createElement(CardHeader, { userName: comment.user.displayName }),
+            React.createElement(CardText, { text: comment.text }),
+            React.createElement(CardInfo, { createdDate: formatDate(comment.created, locale) }),
+            isOwner && canReply ? React.createElement(
+                'div',
+                { className: classes.commentActions },
+                React.createElement(ActionButton, {
+                    iconType: 'edit',
+                    onClick: function onClick() {
+                        return onEdit(comment);
+                    }
+                }),
+                React.createElement(ActionButton, {
+                    iconType: 'reply',
+                    onClick: function onClick() {
+                        return onReply(comment);
+                    }
+                }),
+                React.createElement(ActionButton, {
+                    iconType: 'delete',
+                    onClick: onDelete
+                })
+            ) : canReply && React.createElement(ActionButton, {
+                iconType: 'reply',
+                onClick: function onClick() {
+                    return onReply(comment);
+                }
+            })
+        ),
+        dialogIsOpen && React.createElement(DeleteDialog, {
+            title: i18n.t('Delete comment'),
+            text: i18n.t('Are you sure you want to delete this comment?'),
+            onDelete: function onDelete() {
+                return onDeleteConfirm(comment);
+            },
+            onCancel: onDeleteCancel
+        })
+    );
+};
+
+Comment.propTypes = {
+    classes: PropTypes.object.isRequired,
+    comment: PropTypes.object.isRequired,
+    isOwner: PropTypes.bool.isRequired,
+    locale: PropTypes.string.isRequired,
+    onEdit: PropTypes.func.isRequired,
+    onReply: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    dialogIsOpen: PropTypes.bool.isRequired,
+    onDeleteConfirm: PropTypes.func.isRequired,
+    onDeleteCancel: PropTypes.func.isRequired
+};
+
+export default withStyles(styles)(Comment);
---
+++ components/Lists/InterpretationsList.js
@@ -0,0 +1,66 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import i18n from '@dhis2/d2-i18n';
+import Link from '../Link/Link';
+import Interpretation from '../Interpretation/Interpretation';
+import { haveReadAccess } from '../../authorization/auth';
+import styles from './styles/InterpretationsList.style';
+
+export var interpretationsToShowOnInit = 5;
+
+export var InterpretationsList = function InterpretationsList(_ref) {
+    var classes = _ref.classes,
+        d2 = _ref.d2,
+        model = _ref.model,
+        userGroups = _ref.userGroups,
+        interpretations = _ref.interpretations,
+        onSelect = _ref.onSelect,
+        onChange = _ref.onChange,
+        isExpanded = _ref.isExpanded,
+        toggleShowAllInterpretations = _ref.toggleShowAllInterpretations;
+
+    var filteredItems = interpretations.filter(function (item) {
+        return haveReadAccess(d2, userGroups, item) && item;
+    });
+
+    var listItems = isExpanded ? filteredItems : filteredItems.slice(-interpretationsToShowOnInit);
+
+    return listItems.length ? React.createElement(
+        Fragment,
+        null,
+        listItems.length > interpretationsToShowOnInit && React.createElement(Link, {
+            label: (isExpanded ? i18n.t('Hide') : i18n.t('Show')) + ' previous interpretations',
+            onClick: toggleShowAllInterpretations
+        }),
+        listItems.map(function (item) {
+            return React.createElement(Interpretation, {
+                model: model,
+                userGroups: userGroups,
+                haveReadAccess: haveReadAccess(d2, userGroups, item),
+                key: item.id,
+                interpretation: item,
+                onChange: onChange,
+                onSelect: onSelect,
+                extended: false
+            });
+        })
+    ) : React.createElement(
+        'div',
+        { className: classes.emptyList },
+        i18n.t("No interpretations")
+    );
+};
+
+InterpretationsList.propTypes = {
+    classes: PropTypes.object.isRequired,
+    d2: PropTypes.object.isRequired,
+    model: PropTypes.object.isRequired,
+    interpretations: PropTypes.array.isRequired,
+    onSelect: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    isExpanded: PropTypes.bool.isRequired,
+    toggleShowAllInterpretations: PropTypes.func.isRequired
+};
+
+export default withStyles(styles)(InterpretationsList);
